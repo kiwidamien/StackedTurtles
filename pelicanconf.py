41733e998cca5c80864682d57c10957fc6b39d9b
kiwidamien <djmartin@ucdavis.edu>
2018-10-10 20:40:40 -0700

    corrected SITENAME in conf file

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 
 AUTHOR = u'Damien Martin'
 SITENAME = u'Stacked Turtles'
-SITEURL = 'https://kiwidamien.github.io/'
+SITEURL = 'https://kiwidamien.github.io'
 
 PATH = 'content'
 
